4692263d7384c2e580e086762ddd5fa80d6203a1
Timothy Tavarez <timothy.tavarez@muse.codes>
2017-03-05 14:18:36 -0700

    Mess with strings.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("This is a functional .NET Core application.");
+            var name = "Mariana";
+            
+            string initiallyNull;
+            initiallyNull = "No longer null!";
+
+            string emptyString1 = "";
+            string emptyString2 = String.Empty;
+
+            string friends = "abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine($"emptyString1 is currently empty. Let's add some friends from the alphabet: {emptyString1+friends}. Ahh, how lovely to no longer be lonely, right {name}?");
+
+            Console.WriteLine($"Let's bump up the volume: {friends.ToUpper()}");
+
+            foreach (char letter in name) {
+                Console.Write(letter + " ");
+            }
+
         }
     }
 }
