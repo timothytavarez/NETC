6426e9ea283275eea20a9c4e2ba1fb87047d936a
Timothy Tavarez <timothy.tavarez@muse.codes>
2017-03-04 21:34:17 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a functional .NET Core application.");
+        }
+    }
+}
